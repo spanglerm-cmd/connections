c89b73312d59057c5f235a11a0fcf1c547a7b96d
spanglerm-cmd <spangler_m@my.westcler.org>
2026-09-01 12:45:22 -0400

    Add files via upload

---
+++ puzzles.js
@@ -0,0 +1,20 @@
+const PUZZLES = {
+  1: {
+    id: 1, title: "Connections Puzzle 1",
+    groups: [
+      {words:["MARS","SATURN","JUPITER","NEPTUNE"], connection:"Planets"},
+      {words:["CHEETAH","LEOPARD","LION","TIGER"], connection:"Animals"},
+      {words:["FERRARI","MERCURY","TESLA","JAGUAR"], connection:"Cars"},
+      {words:["EARTH","SUN","OIL","COAL"], connection:"Things We Get Energy From"}
+    ]
+  },
+  2: {
+    id: 2, title: "Connections Puzzle 2",
+    groups: [
+      {words:["GREEN","PURPLE","RED","BROWN"], connection:"Colors"},
+      {words:["APPLE","CHERRY","STRAWBERRY","RADISH"], connection:"Red Fruits and Vegetables"},
+      {words:["GRAPES","PLUMS","CABBAGE","BEETS"], connection:"Purple Fruits and Vegetables"},
+      {words:["LIMES","PEARS","KIWI","CUCUMBERS"], connection:"Green Fruits and Vegetables"}
+    ]
+  }
+};
